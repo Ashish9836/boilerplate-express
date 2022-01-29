0a5fdb4ff7ed5efbf8f6b5c0a13ef3994ae1b7f4
Ashish Kumar <78853800+Ashish9836@users.noreply.github.com>
2022-01-29 15:04:53 +0000

    just something to learn

--- myApp.js
+++ myApp.js
@@ -4,6 +4,27 @@ var app = express();
 
 
 
+// making middle ware 
+function mid (req,res,next){
+console.log(`${req.method} ${req.path} - ${req.ip}`);
+next();
+
+}
+
+// using middleware for all requests
+app.use(mid);
+
+
+
+// app.post(mid) for using middleware for post requests
+
+
+
+app.get("/",(req,res)=>{
+res.sendFile('views/index.html',{ root : __dirname});
+})
+
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
@@ -11,29 +32,28 @@ var app = express();
 
 
 
+app.get("/json",(req,res)=>{
+const obj = {"message": "Hello json"};
+const obj1 = {"message":"HELLO JSON"};
+if(process.env.MESSAGE_STYLE === 'uppercase'){
+  return res.json(obj1)
+}
+return res.json(obj);
+})
 
 
+// using middleware for a specific route
+app.get("/now",function(req,res,next){
+  req.time = new Date().toString();
+  next();
+},(req,res)=>{
+  res.json({"time":req.time});
+})
+
+app.get('/:words/echo', (req,res)=>{
+return res.json({"echo":req.params.words});
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log("Hello World");
  module.exports = app;
